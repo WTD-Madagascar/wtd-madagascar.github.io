7b33879d45edc923e36f5e68ecf844b03f373b43
WTD-Madagascar <workbilly360@gmail.com>
2026-01-26 08:59:22 +0100

    Create institutions.js

---
+++ assets/data/institutions.js
@@ -0,0 +1,18 @@
+{
+  "institutions": [
+    {
+      "id": "mairie_tana",
+      "name": "Mairie d’Antananarivo",
+      "type": "institution",
+      "contact": "contact@mairie-tana.mg",
+      "partner": false
+    },
+    {
+      "id": "association_handigasy",
+      "name": "Association Handigasy",
+      "type": "association",
+      "contact": "handigasy@gmail.com",
+      "partner": true
+    }
+  ]
+}
